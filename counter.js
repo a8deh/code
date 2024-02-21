50afb6250d26a6fbd0ad57b3cf5b414108e32905
Abdelhadi HADDADI <126632654+a8deh@users.noreply.github.com>
2024-02-21 22:10:41 +0100

    Update counter.js

--- counter.js
+++ counter.js
@@ -12,8 +12,10 @@ var createCounter = function(init) {
 };
 
 /**
+
  * const counter = createCounter(5)
  * counter.increment(); // 6
  * counter.reset(); // 5
  * counter.decrement(); // 4
+ 
  */
